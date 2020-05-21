30abe93f046b2e2f4048942fddbb6bd160dfb417
Adam <adammonast@Adams-MBP.attlocal.net>
2020-05-21 15:49:09 -0400

    4/5 MVP

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -2,10 +2,24 @@
 Input: a List of integers
 Returns: a List of integers
 '''
+
+
 def moving_zeroes(arr):
     # Your code here
+    """
+    From GeeksforGeeks: Enumerate() method adds a counter to an iterable and returns it in a form of enumerate object. This enumerate object can then be used directly in for loops or be converted into a list of tuples using list() method
 
-    pass
+    """
+
+    # iterate through the array using enumeration
+    for index, i in enumerate(arr):
+        # if the arr index is not 0, call .pop() on the element, then call .insert() to insert it in its proper place
+        if arr[index] is not 0:
+            # removes/returns last value from the list or the given index value
+            arr.pop(index)
+            # inserts a given element at a given index in a list
+            arr.insert(0, i)
+    return arr
 
 
 if __name__ == '__main__':
--- single_number/single_number.py
+++ single_number/single_number.py
@@ -2,10 +2,23 @@
 Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
+
+
 def single_number(arr):
     # Your code here
-
-    pass
+    # keep an array to hold numbers we see in the numbers array
+    no_duplicates = []
+    # iterate through numbers
+    for x in arr:
+        # check to see if the number is already in the created array
+        if x not in no_duplicates:
+            no_duplicates.append(x)
+        # if it is, remove it from array
+        else:
+            no_duplicates.remove(x)
+    # when iteration is done, the only number in the array is the odd number out
+    # return odd number
+    return no_duplicates[0]
 
 
 if __name__ == '__main__':
